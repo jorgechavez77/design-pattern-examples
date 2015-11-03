136d9def653640a585e2cdb6f297796979e50e16
jachavez <jachavez@axcess-financial.com>
2015-11-03 10:51:08 -0500

    IO test

---
+++ src/test/java/org/demo/design/pattern/decorator/io/InputTest.java
@@ -0,0 +1,32 @@
+package org.demo.design.pattern.decorator.io;
+
+import java.io.BufferedInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.Test;
+
+public class InputTest {
+
+	@Test
+	public void test() {
+		InputStream test = ClassLoader.getSystemResourceAsStream("test.txt");
+
+		int c;
+		try {
+			InputStream in = new LowerCaseInputStream(new BufferedInputStream(
+					test));
+			while ((c = in.read()) >= 0) {
+				System.out.print((char) c);
+			}
+
+			in.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
