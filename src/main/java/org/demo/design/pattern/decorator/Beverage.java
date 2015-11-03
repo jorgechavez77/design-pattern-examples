1906b5c84e7ee6e928c3c8b88158027aefedf18c
jachavez <jachavez@axcess-financial.com>
2015-11-03 10:28:16 -0500

    More

--- src/main/java/org/demo/design/pattern/decorator/Beverage.java
+++ src/main/java/org/demo/design/pattern/decorator/Beverage.java
@@ -2,10 +2,19 @@ package org.demo.design.pattern.decorator;
 
 public abstract class Beverage {
 
-	protected String description = "Unkown Beverage";
+	String description = "Unkown Beverage";
+	Size size;
 
 	public String getDescription() {
 		return description;
+	}
+
+	public Size getSize() {
+		return size;
+	}
+
+	public void setSize(Size size) {
+		this.size = size;
 	}
 
 	public abstract double cost();
---
+++ src/main/java/org/demo/design/pattern/decorator/Size.java
@@ -0,0 +1,7 @@
+package org.demo.design.pattern.decorator;
+
+public enum Size {
+
+	VENTI, GRANDE, TALL
+	
+}
