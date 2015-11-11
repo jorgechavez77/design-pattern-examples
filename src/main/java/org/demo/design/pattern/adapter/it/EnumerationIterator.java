212f563396e81e60cc2f2b5dc0216213831f28aa
jachavez <jachavez@axcess-financial.com>
2015-11-11 16:28:08 -0500

    More classes for Adapter pattern

---
+++ src/main/java/org/demo/design/pattern/adapter/it/EnumerationIterator.java
@@ -0,0 +1,29 @@
+package org.demo.design.pattern.adapter.it;
+
+import java.util.Enumeration;
+import java.util.Iterator;
+
+public class EnumerationIterator<E> implements Iterator<E> {
+
+	private Enumeration<E> enumeration;
+
+	public EnumerationIterator(Enumeration<E> enumeration) {
+		this.enumeration = enumeration;
+	}
+
+	@Override
+	public boolean hasNext() {
+		return enumeration.hasMoreElements();
+	}
+
+	@Override
+	public E next() {
+		return enumeration.nextElement();
+	}
+
+	@Override
+	public void remove() {
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ src/test/java/org/demo/design/pattern/adapter/IteratorEnumerationTest.java
@@ -0,0 +1,24 @@
+package org.demo.design.pattern.adapter;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.demo.design.pattern.adapter.it.IteratorEnumeration;
+import org.junit.Test;
+
+public class IteratorEnumerationTest {
+
+	@Test
+	public void test() {
+		String[] array = { "A", "B", "C" };
+		List<String> list = Arrays.asList(array);
+
+		IteratorEnumeration<String> ie = new IteratorEnumeration<>(
+				list.iterator());
+
+		while (ie.hasMoreElements()) {
+			System.out.println(ie.nextElement());
+		}
+	}
+
+}
